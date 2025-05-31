f542d76ffca577ef8178f2f7feb937a4c1905a50
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-05-31 13:25:40 -0400

    feature: add authService to handle login.

--- src/interfaces/auth.ts
+++ src/interfaces/auth.ts
@@ -3,3 +3,17 @@ export interface UserCredentials {
 	password: string;
 	rememberMe: boolean;
 }
+
+export interface LoginRequest {
+	email: string;
+	password: string;
+}
+
+export interface LoginResponse {
+	token: string;
+}
+
+export interface ApiError {
+	message: string;
+	statusCode: number;
+}
