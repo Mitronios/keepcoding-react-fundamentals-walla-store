0323a3c6a118d12e1da6d70085f41c50023ef5bc
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-05-31 23:36:47 -0400

    feature: add not found page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,10 @@
 const NotFound = () => {
-	return <div>Notfound</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen text-center">
+			<h1 className="text-4xl font-bold mb-4">404</h1>
+			<p className="text-xl text-gray-600">Página no encontrada</p>
+		</div>
+	);
 };
 
 export default NotFound;
